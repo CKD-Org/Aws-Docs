53b943c6abe8cae3cf64f68889c5a29e6880a0f8
charankowndinya <55056562+charankowndinya@users.noreply.github.com>
2019-09-12 18:26:59 +0530

    Update instances-list.sh

--- instances-list.sh
+++ instances-list.sh
@@ -4,3 +4,4 @@ echo Listing InstanceID, ImageID, InstanceType, PrivateIP, Tags
 Ins=$(aws ec2 describe-instances  --query 'Reservations[*].Instances[*].[InstanceId,ImageId,InstanceType,PrivateIpAddress,Tags]' --output text)
 echo $Ins
 updated by shanmukha
+hi shanmukha this charan updating for u
