0a6d0dde804daef480b73c632cb544ba1fcf057c
paytmuser <nath.shanmukha@gmail.com>
2019-09-12 14:19:25 +0530

    updated by shanmukha

--- instances-list.sh
+++ instances-list.sh
@@ -3,4 +3,4 @@
 echo Listing InstanceID, ImageID, InstanceType, PrivateIP, Tags
 Ins=$(aws ec2 describe-instances  --query 'Reservations[*].Instances[*].[InstanceId,ImageId,InstanceType,PrivateIpAddress,Tags]' --output text)
 echo $Ins
-
+updated by shanmukha
